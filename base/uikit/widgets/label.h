562e48ab3b80ff22945443c32229f46ec2e629ea
Siraj Razick <siraj@kde.org>
2015-04-03 00:18:12 +0530

    [uikit] Add method to control the label text alignment
    Adds text alignment property to label class so that we can
    render label strings with different alignments.

--- base/uikit/widgets/label.h
+++ base/uikit/widgets/label.h
@@ -23,23 +23,22 @@ public:
   virtual ~Label();
 
   virtual void set_label(const QString &a_txt);
-
   virtual QString label() const;
 
-  virtual QRectF boundingRect() const;
 
   virtual void set_size(const QSizeF &_asize);
-
   virtual void set_font_size(uint a_pixelSize);
 
   virtual QSizeF sizeHint(Qt::SizeHint which, const QSizeF &a_constraint) const;
-
+  virtual QRectF boundingRect() const;
   virtual void setGeometry(const QRectF &a_rect);
-
   virtual QRectF contents_bounding_rect() const;
 
   virtual void set_label_style(const QColor &a_backgroundColor,
                              const QColor &a_textColor);
+
+  virtual int alignment();
+  virtual void set_alignment(int a_alignment);
 
 Q_SIGNALS:
   void contentBoundingRectChaned();
