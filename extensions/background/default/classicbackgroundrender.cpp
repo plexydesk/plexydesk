55b9832c0f6988da240bfa0d727dd903f69ecb65
Siraj Razick <siraj@plexydesk.org>
2015-06-08 13:07:31 +0530

    [ui] Only activat the focus chain when left button is pressed.
    Focus chain is activated for all the mouse events, make sure we only
    activates it when the window gets focused.

--- extensions/background/default/classicbackgroundrender.cpp
+++ extensions/background/default/classicbackgroundrender.cpp
@@ -95,12 +95,6 @@ void ClassicBackgroundRender::setSeamLessMode(bool value) {
   update();
 }
 
-void ClassicBackgroundRender::mousePressEvent(QGraphicsSceneMouseEvent *event) {
-  qDebug() << Q_FUNC_INFO << "Mouse presesed on the background";
-  setFocus(Qt::MouseFocusReason);
-  UIKit::Window::mousePressEvent(event);
-}
-
 void ClassicBackgroundRender::paint_view(QPainter *painter,
                                          const QRectF &rect /*rect*/) {
   if (mSeamLessMode) {
