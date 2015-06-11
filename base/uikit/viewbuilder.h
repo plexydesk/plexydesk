dd672e9b97d25977e4fd6667a0b23df160dfff72
Siraj Razick <siraj@plexydesk.org>
2015-06-11 15:30:51 +0530

    [ui new viewbuilder class.
    Adds a new view builder class. to create ui's easily

---
+++ base/uikit/viewbuilder.h
@@ -0,0 +1,41 @@
+#ifndef VIEWBUILDER_H
+#define VIEWBUILDER_H
+
+#include <widget.h>
+#include <window.h>
+
+namespace UIKit {
+class Label;
+class Button;
+class ImageButton;
+class LineEdit;
+
+class ViewBuilder {
+  typedef enum {
+    kAlignNone = 0,
+    kAlignLeft = 1,
+    kAlignRight = 2,
+    kAlignCenter = 3,
+  } Alignment;
+
+public:
+  explicit ViewBuilder(Widget *a_window);
+  virtual ~ViewBuilder();
+
+  virtual void add_column(int a_count);
+  virtual void add_sub_column(int a_column_index, int a_row_index, int a_count);
+
+  virtual void add_row(int a_count);
+  virtual void add_sub_row(int a_index, int a_count);
+
+  virtual int add_button(int a_parent_id, const std::string &a_label) const;
+  virtual Widget *button(int a_id);
+
+  virtual Widget *ui() const;
+
+private:
+  class PrivateViewBuilder;
+  PrivateViewBuilder *const d;
+};
+}
+#endif // VIEWBUILDER_H
