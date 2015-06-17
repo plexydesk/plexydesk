38a7bc06d5ff20254e7750303591cd08e860af5c
Siraj Razick <siraj@plexydesk.org>
2015-06-17 12:19:51 +0530

    [ui] Update Viewbuilder to support differnt widget types.
    Support building new widget types.

--- base/uikit/viewbuilder.cpp
+++ base/uikit/viewbuilder.cpp
@@ -8,22 +8,22 @@ namespace UIKit {
 class Column {};
 class Row {};
 
-//typedef std::pair<int, int> GridPos;
+typedef std::pair<int, int> GridPos;
 
 class ViewBuilder::PrivateViewBuilder {
 public:
-  PrivateViewBuilder(Widget *a_window)
-      : m_view_parent(a_window), m_row_count(0), m_column_count(0) {
-    m_content_frame = new Widget(m_view_parent);
+   PrivateViewBuilder(Widget *a_window)
+      : m_row_count(0), m_column_count(0) {
+    m_content_frame = new Widget(a_window);
+    build_ui_map();
   }
 
-  ~PrivateViewBuilder() {
-    if (m_content_frame)
-      delete m_content_frame;
-  }
+  void build_ui_map();
 
-  Widget *m_view_parent;
-  Widget *m_content_frame;
+  float get_layout_width();
+  float get_layout_height();
+
+  ~PrivateViewBuilder() {}
 
   int m_row_count;
   int m_column_count;
@@ -31,7 +31,10 @@ public:
   std::map<int, int> m_column_data;
   std::map<int, int> m_row_data;
 
-  //std::map<GridPos, int> m_grid_data;
+  std::map<GridPos, Widget *> m_widget_grid;
+  std::map<std::string, int> m_ui_dict;
+
+  UIKit::Widget *m_content_frame;
 };
 
 ViewBuilder::ViewBuilder(Widget *a_window)
@@ -43,23 +46,68 @@ void ViewBuilder::add_column(int a_count) { d->m_column_count = a_count; }
 
 void ViewBuilder::add_sub_column(int a_column_index, int a_row_index,
                                  int a_count) {
-  if ((a_column_index > d->m_column_count) || (a_column_index < 0))
-    return;
+}
 
-  if ((a_row_index > d->m_row_count) || a_row_index < 0)
+void ViewBuilder::add_row(int a_row_count) {
+  d->m_row_count = a_row_count;
+}
+
+void ViewBuilder::add_sub_row(int a_index, int a_count) {
+}
+
+Widget *ViewBuilder::ui() const
+{
+  return d->m_content_frame;
+}
+
+void ViewBuilder::add_new_widget_at(int a_col, int a_row,
+                                    const ViewProperties &a_props)
+{
+  Widget *widget = new Widget(d->m_content_frame);
+  GridPos pos(a_col, a_row);
+  d->m_widget_grid[pos]= widget;
+}
+
+void ViewBuilder::add_new_button_at(int a_col, int a_row,
+                                    const ViewProperties &a_props) {
+}
+
+void ViewBuilder::add_new_label_at(int a_col, int a_row,
+                                   const ViewProperties &a_props) {
+}
+
+void ViewBuilder::add_widget(int a_col, int a_row,
+                             const std::string &a_widget,
+                             const ViewProperties &a_properties) {
+  switch(d->m_ui_dict[a_widget]) {
+    case kWidget:
+      add_new_widget_at(a_col, a_row, a_properties);
+    break;
+    case kButton:
+      add_new_widget_at(a_col, a_row, a_properties);
+    default:
       return;
-
-  //GridPos key_index = std::make_pair(a_column_index, a_row_index);
+  }
 }
 
-void ViewBuilder::add_row(int a_row_count) {}
-
-void ViewBuilder::add_sub_row(int a_index, int a_count) {}
-
-int ViewBuilder::add_button(int a_parent_id, const std::string &a_label) const {
+void ViewBuilder::PrivateViewBuilder::build_ui_map() {
+   m_ui_dict["widget"] = ViewBuilder::kWidget;
+   m_ui_dict["button"] = ViewBuilder::kButton;
+   m_ui_dict["image_button"] = ViewBuilder::kImageButton;
+   m_ui_dict["label"] = ViewBuilder::kLabel;
+   m_ui_dict["line_edit"] = ViewBuilder::kLineEdit;
+   m_ui_dict["text_edit"] = ViewBuilder::kTextEdit;
+   m_ui_dict["progress_bar"] = ViewBuilder::kProgressBar;
+   m_ui_dict["dial"] = ViewBuilder::kDial;
+   m_ui_dict["model_view"] = ViewBuilder::kModelView;
+   m_ui_dict["divider"] = ViewBuilder::kDivider;
 }
 
-Widget *ViewBuilder::button(int a_id) {}
+float ViewBuilder::PrivateViewBuilder::get_layout_width() {
+  return 0;
+}
 
-Widget *ViewBuilder::ui() const { return d->m_content_frame; }
+float ViewBuilder::PrivateViewBuilder::get_layout_height() {
+  return 0;
+}
 }
--- base/uikit/viewbuilder.h
+++ base/uikit/viewbuilder.h
@@ -2,6 +2,7 @@
 #define VIEWBUILDER_H
 
 #include <iostream>
+#include <variant.h>
 
 namespace UIKit {
 class Widget;
@@ -11,6 +12,8 @@ class Button;
 class ImageButton;
 class LineEdit;
 
+typedef std::vector<std::pair<std::string, std::string>> ViewProperties;
+
 class ViewBuilder {
   typedef enum {
     kAlignNone = 0,
@@ -18,6 +21,19 @@ class ViewBuilder {
     kAlignRight = 2,
     kAlignCenter = 3,
   } Alignment;
+
+ typedef enum {
+   kWidget = 0,
+   kButton,
+   kImageButton,
+   kLabel,
+   kLineEdit,
+   kTextEdit,
+   kProgressBar,
+   kDial,
+   kDivider,
+   kModelView
+ } ViewIdentifier;
 
 public:
   explicit ViewBuilder(Widget *a_window);
@@ -29,11 +45,15 @@ public:
   virtual void add_row(int a_count);
   virtual void add_sub_row(int a_index, int a_count);
 
-  virtual int add_button(int a_parent_id, const std::string &a_label) const;
-  virtual Widget *button(int a_id);
-
   virtual Widget *ui() const;
 
+  virtual void add_widget(int a_col, int a_row,
+                          const std::string &a_widget,
+                          const ViewProperties &a_properties);
+protected:
+  void add_new_widget_at(int a_col, int a_row, const ViewProperties &a_props);
+  void add_new_button_at(int a_col, int a_row, const ViewProperties &a_props);
+  void add_new_label_at(int a_col, int a_row, const ViewProperties &a_props);
 private:
   class PrivateViewBuilder;
   PrivateViewBuilder *const d;
