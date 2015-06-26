fabcb988f91aaff2bdc019a8cce9ff2fa68d04b2
Siraj Razick <siraj@plexydesk.org>
2015-06-27 03:05:56 +0530

    [background] Remove old background handling code.
    Work in progress background handler.

--- extensions/background/default/desktopwindow.cpp
+++ extensions/background/default/desktopwindow.cpp
@@ -1,5 +1,59 @@
 #include "desktopwindow.h"
 
+#include <QImage>
+#include <QPainter>
+
+class DesktopWindow::PrivateDesktopWindow {
+public:
+  PrivateDesktopWindow()
+      : m_background_buffer(0), m_background_width(0), m_background_height(0) {}
+  ~PrivateDesktopWindow() {
+    if (m_background_buffer)
+      free(m_background_buffer);
+  }
+
+  unsigned char *m_background_buffer;
+  int m_background_width;
+  int m_background_height;
+};
+
+void DesktopWindow::reset_window_background() {
+  memset(p_window->m_background_buffer, 0,
+         (4 * p_window->m_background_height * p_window->m_background_width));
+}
+
 DesktopWindow::DesktopWindow()
-{
+    : UIKit::Window(0), p_window(new PrivateDesktopWindow) {
+  set_window_type(UIKit::Window::kFramelessWindow);
+
+  on_window_resized([this](Window *a_window, int a_width, int a_height) {
+    if (p_window->m_background_buffer) {
+      reset_window_background();
+      p_window->m_background_width = a_width;
+      p_window->m_background_height = a_height;
+      p_window->m_background_buffer = (unsigned char *)realloc(
+          p_window->m_background_buffer, (a_width, a_height * 4));
+      reset_window_background();
+    }
+  });
+}
+
+DesktopWindow::~DesktopWindow() { delete p_window; }
+
+void DesktopWindow::set_background(const std::string &a_image_name) {
+
+}
+
+void DesktopWindow::paint_view(QPainter *a_ctx, const QRectF &a_rect) {
+  if (!p_window->m_background_buffer) {
+    UIKit::Window::paint_view(a_ctx, a_rect);
+    return;
+  }
+
+  a_ctx->drawImage(a_rect, QImage(p_window->m_background_buffer,
+                                  p_window->m_background_width,
+                                  p_window->m_background_height,
+                                  QImage::Format_ARGB32_Premultiplied));
+
+  UIKit::Window::paint_view(a_ctx, a_rect);
 }
