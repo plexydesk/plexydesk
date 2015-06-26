d0faab452ae25e9b2f7d586d9449b95c693a2a00
Siraj Razick <siraj@plexydesk.org>
2015-06-27 03:01:04 +0530

    [data] benchmark utilities.
    A simple benchmark utility to check the elapsed time between functions.

---
+++ base/datastore/lib/px_bench.h
@@ -0,0 +1,19 @@
+#ifndef PX_BENCH_H
+#define PX_BENCH_H
+
+#include <chrono>
+
+typedef struct {
+  std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
+  std::chrono::time_point<std::chrono::high_resolution_clock> end_time;
+  std::chrono::duration<double> elapsed_seconds;
+} PxBenchData;
+
+extern void px_bench_run(PxBenchData *a_bench_data);
+
+extern void px_bench_stop(PxBenchData *a_bench_data);
+
+extern void px_bench_print(PxBenchData *a_bench_data,
+                                  const char *a_func_name);
+
+#endif // PX_BENCH_H
---
+++ extensions/background/default/desktopwindow.cpp
@@ -0,0 +1,5 @@
+#include "desktopwindow.h"
+
+DesktopWindow::DesktopWindow()
+{
+}
