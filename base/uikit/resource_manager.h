1785e316e14cfcbccad6d2ea10fae6f853f9ff5f
Siraj Razick <siraj@plexydesk.org>
2015-06-26 13:52:08 +0530

    [ui] Add support for Loading Color schemes from Resource Manager.
    Adds support for loading color schemes from the resource manager so that
    we can have a fast color table for the plugins and the ui core.

--- base/uikit/resource_manager.h
+++ base/uikit/resource_manager.h
@@ -20,9 +20,26 @@ public:
   static QPixmap icon(const QString &a_name, const QString &a_resolution);
   virtual QPixmap drawable(const QString &a_fileName, const QString &a_dpi);
 
+  typedef enum {
+    kDarkPrimaryColor = 1,
+    kPrimaryColor,
+    kLightPrimaryColor,
+    kTextBackground,
+    kTextColor,
+    kSecondryTextColor,
+    kDividerColor,
+    kAccentColor
+  } ColorName;
+
+  static const char *color(ColorName a_name);
+  virtual void set_color_scheme(const std::string &a_name);
+  virtual std::string color_scheme() const;
+  virtual const char *color_code(ColorName a_name);
+
 private:
   void scane_resources();
   StylePtr default_desktop_style();
+  void load_default_color_values();
 
   class ThemepackLoaderPrivate;
   ThemepackLoaderPrivate *const d;
