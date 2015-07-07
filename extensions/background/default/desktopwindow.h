a08252875df3b728704fdd94c1880606e967f61d
Siraj Razick <siraj@plexydesk.org>
2015-07-07 06:05:22 +0530

    [background] Support seamlessmode.
    Adds support for seamless mode.

--- extensions/background/default/desktopwindow.h
+++ extensions/background/default/desktopwindow.h
@@ -11,8 +11,10 @@ public:
   virtual ~DesktopWindow();
 
   void set_background(const QString &a_image_name);
-
   void reset_window_background();
+
+  bool is_seamless();
+  void set_seamless(bool mode = true);
 
 protected:
   void paint_view(QPainter *a_ctx, const QRectF &a_rect);
