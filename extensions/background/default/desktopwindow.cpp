e252f81c67eee4e07764bf87a9452613792f408a
Siraj Razick <siraj@plexydesk.org>
2015-07-07 06:05:22 +0530

    [background] Support seamlessmode.
    Adds support for seamless mode.

--- extensions/background/default/desktopwindow.cpp
+++ extensions/background/default/desktopwindow.cpp
@@ -6,7 +6,8 @@
 class DesktopWindow::PrivateDesktopWindow {
 public:
   PrivateDesktopWindow()
-      : m_background_buffer(0), m_background_width(0), m_background_height(0) {}
+      : m_background_buffer(0), m_background_width(0), m_background_height(0),
+        m_seamless(0) {}
   ~PrivateDesktopWindow() {
     if (m_background_buffer)
       free(m_background_buffer);
@@ -16,12 +17,17 @@ public:
   unsigned char *m_background_buffer;
   int m_background_width;
   int m_background_height;
+  bool m_seamless;
 };
 
 void DesktopWindow::reset_window_background() {
   memset(o_window->m_background_buffer, 0,
          (4 * o_window->m_background_height * o_window->m_background_width));
 }
+
+bool DesktopWindow::is_seamless() { return o_window->m_seamless; }
+
+void DesktopWindow::set_seamless(bool mode) { o_window->m_seamless = mode; }
 
 DesktopWindow::DesktopWindow()
     : CherryKit::Window(0), o_window(new PrivateDesktopWindow) {
@@ -52,25 +58,22 @@ void DesktopWindow::set_background(const QString &a_image_name) {
   p_window->m_background_buffer = background.bits();
   */
 
-  o_window->m_background_texture.load(a_image_name);
+  if (!o_window->m_background_texture.load(a_image_name))
+      qDebug() << Q_FUNC_INFO << "Failed to load image";
 
   update();
 }
 
 void DesktopWindow::paint_view(QPainter *a_ctx, const QRectF &a_rect) {
-  /*
-  if (!p_window->m_background_buffer) {
-    CherryKit::Window::paint_view(a_ctx, a_rect);
+  if (o_window->m_seamless) {
+    a_ctx->setBackgroundMode(Qt::TransparentMode);
+    a_ctx->setCompositionMode(QPainter::CompositionMode_Source);
+    a_ctx->fillRect(a_rect, Qt::transparent);
+
+    Widget::paint_view(a_ctx, a_rect);
+
     return;
   }
-
-  a_ctx->drawImage(a_rect, QImage(p_window->m_background_buffer,
-                                  p_window->m_background_width,
-                                  p_window->m_background_height,
-                                  QImage::Format_ARGB32_Premultiplied));
-
-  CherryKit::Window::paint_view(a_ctx, a_rect);
-  */
 
   a_ctx->drawImage(a_rect, o_window->m_background_texture);
 }
