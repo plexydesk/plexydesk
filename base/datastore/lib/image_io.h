69a094551ac4ab65951b0c2934f8bb4a744350ff
Siraj Razick <siraj@plexydesk.org>
2015-07-09 06:40:19 +0530

    add missing headers
    Adds license headers

--- base/datastore/lib/image_io.h
+++ base/datastore/lib/image_io.h
@@ -1,11 +1,33 @@
+/*******************************************************************************
+* This file is part of PlexyDesk.
+*  Maintained by : Siraj Razick <siraj@plexydesk.org>
+*  Authored By  :
+*
+*  PlexyDesk is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  PlexyDesk is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with PlexyDesk. If not, see <http://www.gnu.org/licenses/lgpl.html>
+*******************************************************************************/
 #ifndef IMAGE_IO_H
 #define IMAGE_IO_H
 
-
-class image_io
-{
+namespace ck {
+class image_io {
 public:
-    image_io();
+  image_io();
+  virtual ~image_io();
+
+  virtual void set_buffer(unsigned char *a_buffer);
+  virtual void scale(int a_method);
 };
+}
 
 #endif // IMAGE_IO_H
