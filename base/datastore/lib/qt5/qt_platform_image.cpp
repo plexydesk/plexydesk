2a9df3176e2c16a0b4a3fe966e1b7ab127a55340
Siraj Razick <siraj@plexydesk.org>
2015-07-12 19:20:11 +0530

    [data] New Platform Impl for Image IO class.
    Qt backend support for Image IO Class. provide
    a QImage based image loader.

---
+++ base/datastore/lib/qt5/qt_platform_image.cpp
@@ -0,0 +1,7 @@
+#include "qt_platform_image.h"
+
+qt_platform_image::qt_platform_image()
+{
+
+}
+
---
+++ base/datastore/lib/qt5/qt_platform_image.h
@@ -0,0 +1,11 @@
+#ifndef QT_PLATFORM_IMAGE_H
+#define QT_PLATFORM_IMAGE_H
+
+
+class qt_platform_image
+{
+public:
+    qt_platform_image();
+};
+
+#endif // QT_PLATFORM_IMAGE_H
