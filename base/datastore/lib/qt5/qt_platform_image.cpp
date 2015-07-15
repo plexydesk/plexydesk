9ce9bb244965c3fc97a2a043f28d1c9fb47b6507
Siraj Razick <siraj@plexydesk.org>
2015-07-16 00:05:02 +0530

    [settings dialog] Async image loading.
    Adds support for async image loading in the background.

--- base/datastore/lib/qt5/qt_platform_image.cpp
+++ base/datastore/lib/qt5/qt_platform_image.cpp
@@ -3,9 +3,10 @@
 #include <QImage>
 #include <QDir>
 #include <QCryptographicHash>
+#include <QCoreApplication>
+
 #include <thread>
 #include <future>
-
 
 namespace cherry_kit {
 
@@ -22,182 +23,204 @@ public:
   std::string m_file_url;
   std::thread m_io_task;
 
-  std::future<void> m_async_notification_result;
+  std::future<io_surface *> m_async_task_result;
+
+  std::promise<io_surface *> m_async_notification_promis;
+
+  std::condition_variable m_notify_condition_variable;
+  std::mutex m_notify_lock_mutex;
+  int m_shared_lock_value;
 };
 
-image_io::platform_image::platform_image() : o(new private_platform_image) {}
+image_io::platform_image::platform_image() : priv(new private_platform_image) {}
 
 image_io::platform_image::~platform_image() {
-    qDebug() << Q_FUNC_INFO;
-    delete o;
-}
-
-static void qtz_set_color_value_at_pos(unsigned char **surface, int width, int x,
-                                int y, int red, int green, int blue,
-                                int alpha) {
-
-
-  if (!(*surface))
-    return;
-
-  (*surface)[((y * width + x) * 4)] = red;
-  (*surface)[((y * width + x) * 4) + 1] = green;
-  (*surface)[((y * width + x) * 4) + 2] = blue;
-  (*surface)[((y * width + x) * 4) + 3] = alpha;
+  //release();
+  delete priv;
 }
 
 void image_io::platform_image::load_from_file(const std::string &a_file_name) {
-  // todo: check if the path is valid first ;
-  o->m_file_url = a_file_name;
+  priv->m_file_url = a_file_name;
+  priv->m_async_task_result = std::async(
+      std::launch::async, &image_io::platform_image::image_decoder, this);
 
-  o->m_async_notification_result = std::async(std::launch::async, [this]() {
-    if (!o->m_on_ready_call) {
-      qWarning() << Q_FUNC_INFO << "Error : NO callback";
-      return;
-    }
-
-    QImage ck_qt_image(o->m_file_url.c_str());
-    io_surface *ck_surface = nullptr;
-
-    if (ck_qt_image.isNull()) {
-      o->m_on_ready_call(ck_surface, image_io::kDataError);
-      return;
-    }
-
-    ck_surface = new io_surface();
-    ck_surface->width = ck_qt_image.width();
-    ck_surface->height = ck_qt_image.height();
-
-    ck_surface->buffer =
-        (unsigned char *)malloc(4 * ck_qt_image.height() * ck_qt_image.width());
-
-    for (int x = 0; x < ck_qt_image.height(); ++x) {
-        QRgb *row_data = (QRgb*)ck_qt_image.scanLine(x);
-        for(int y = 0; y < ck_qt_image.width(); ++y) {
-            QRgb pixel_data = row_data[y];
-
-            int red = qRed(pixel_data);
-            int blue = qBlue(pixel_data);
-            int green = qGreen(pixel_data);
-            int alpha = qAlpha(pixel_data);
-
-            int width = ck_surface->width;
-            ck_surface->buffer[((x * width + y) * 4)] = blue;
-            ck_surface->buffer[((x * width + y) * 4) + 1] = green;
-            ck_surface->buffer[((x * width + y) * 4) + 2] = red;
-            ck_surface->buffer[((x * width + y) * 4) + 3] = alpha;
-        }
-    }
-
-    o->m_on_ready_call(ck_surface, image_io::kSuccess);
-  });
+  QCoreApplication::processEvents();
+  wait_for_signal(this);
 }
 
 void
 image_io::platform_image::load_image_preview(const std::string &a_file_name) {
-  o->m_file_url = a_file_name;
-
-  o->m_async_notification_result = std::async(std::launch::async, [this]() {
-
-    if (!o->m_on_ready_call) {
-      qWarning() << Q_FUNC_INFO << "Error : NO callback";
-      return;
-    }
-
-    bool load_from_cache = o->check_in_cache(o->m_file_url);
-
-    io_surface *ck_surface = nullptr;
-    QImage ck_qt_image;
-
-    if (!load_from_cache) {
-        qDebug() << Q_FUNC_INFO << "Not Found in Cache"
-                 << o->m_file_url.c_str();
-      ck_qt_image = QImage(o->m_file_url.c_str())
-                        .scaled(QSize(128, 128), Qt::KeepAspectRatioByExpanding,
-                                Qt::SmoothTransformation);
-    }
-     else {
-        qDebug() << Q_FUNC_INFO << "Load from Cache";
-      ck_qt_image.load(o->cache_file_name(o->m_file_url));
-    }
-
-    if (!load_from_cache) {
-      QString cache_path =
-          QDir::toNativeSeparators(QDir::homePath() + "/.cherry_io/");
-
-      QString thumbnail_path =
-          QDir::toNativeSeparators(QDir::homePath() + "/.cherry_io/cache/");
-
-      QDir cache_dir(cache_path);
-      QDir thumbnail_dir(thumbnail_path);
-
-      qDebug() << Q_FUNC_INFO << "start saving preview";
-      if (!cache_dir.exists()) {
-        if (!QDir::home().mkdir(cache_path)) {
-          qDebug() << Q_FUNC_INFO << "Failed to Create :" << cache_path;
-          o->m_on_ready_call(ck_surface, image_io::kDataError);
-          return;
-        }
-      }
-
-      if (!thumbnail_dir.exists()) {
-        if (!QDir::home().mkdir(thumbnail_path)) {
-          qDebug() << Q_FUNC_INFO << "Failed to Create :" << thumbnail_path;
-          o->m_on_ready_call(ck_surface, image_io::kDataError);
-          return;
-        }
-      }
-
-      QString cache_file_name = QCryptographicHash::hash(
-          o->m_file_url.c_str(), QCryptographicHash::Md5).toHex();
-      if (!ck_qt_image.save(QDir::toNativeSeparators(thumbnail_path + "/" +
-                                                     cache_file_name) +
-                            ".png")) {
-        qDebug() << Q_FUNC_INFO << "Failed to Create :" << cache_file_name;
-        o->m_on_ready_call(ck_surface, image_io::kDataError);
-        return;
-      }
-    }
-
-    if (ck_qt_image.isNull()) {
-      o->m_on_ready_call(ck_surface, image_io::kDataError);
-      return;
-    }
-
-    ck_surface = new io_surface();
-    ck_surface->width = ck_qt_image.width();
-    ck_surface->height = ck_qt_image.height();
-
-    ck_surface->buffer =
-        (unsigned char *)malloc(4 * ck_qt_image.height() * ck_qt_image.width());
-
-
-    for (int x = 0; x < ck_qt_image.height(); ++x) {
-        QRgb *row_data = (QRgb*)ck_qt_image.scanLine(x);
-        for(int y = 0; y < ck_qt_image.width(); ++y) {
-           // qDebug() << Q_FUNC_INFO << "->" << sizeof (row_data);
-            QRgb pixel_data = row_data[y];
-
-            int red = qRed(pixel_data);
-            int blue = qBlue(pixel_data);
-            int green = qGreen(pixel_data);
-            int alpha = qAlpha(pixel_data);
-
-            int width = ck_surface->width;
-            ck_surface->buffer[((x * width + y) * 4)] = blue;
-            ck_surface->buffer[((x * width + y) * 4) + 1] = green;
-            ck_surface->buffer[((x * width + y) * 4) + 2] = red;
-            ck_surface->buffer[((x * width + y) * 4) + 3] = alpha;
-        }
-    }
-
-    o->m_on_ready_call(ck_surface, image_io::kSuccess);
-  });
+  priv->m_file_url = a_file_name;
+  priv->m_async_task_result =
+      std::async(std::launch::async,
+                 &image_io::platform_image::image_preview_decoder, this);
+  QCoreApplication::processEvents();
+  wait_for_signal(this);
 }
 
 void image_io::platform_image::on_surface_ready(
     std::function<void(io_surface *, buffer_load_status_t)> a_callback) {
-  o->m_on_ready_call = a_callback;
+  priv->m_on_ready_call = a_callback;
+}
+
+void image_io::platform_image::release() {
+  std::unique_lock<std::mutex> notify_lock(priv->m_notify_lock_mutex);
+  priv->m_shared_lock_value = 1;
+  notify_lock.unlock();
+  priv->m_notify_condition_variable.notify_one();
+}
+
+io_surface *image_io::platform_image::image_decoder() {
+  QImage ck_qt_image(priv->m_file_url.c_str());
+  io_surface *ck_surface = nullptr;
+
+  if (ck_qt_image.isNull()) {
+    priv->m_async_notification_promis.set_value(ck_surface);
+    release();
+    return ck_surface;
+  }
+
+  ck_surface = new io_surface();
+  ck_surface->width = ck_qt_image.width();
+  ck_surface->height = ck_qt_image.height();
+
+  ck_surface->buffer =
+      (unsigned char *)malloc(4 * ck_qt_image.height() * ck_qt_image.width());
+
+  for (int x = 0; x < ck_qt_image.height(); ++x) {
+    QRgb *row_data = (QRgb *)ck_qt_image.scanLine(x);
+    for (int y = 0; y < ck_qt_image.width(); ++y) {
+      QRgb pixel_data = row_data[y];
+
+      int red = qRed(pixel_data);
+      int blue = qBlue(pixel_data);
+      int green = qGreen(pixel_data);
+      int alpha = qAlpha(pixel_data);
+
+      int width = ck_surface->width;
+      ck_surface->buffer[((x * width + y) * 4)] = blue;
+      ck_surface->buffer[((x * width + y) * 4) + 1] = green;
+      ck_surface->buffer[((x * width + y) * 4) + 2] = red;
+      ck_surface->buffer[((x * width + y) * 4) + 3] = alpha;
+    }
+  }
+
+  priv->m_async_notification_promis.set_value(ck_surface);
+  release();
+  return ck_surface;
+}
+
+io_surface *image_io::platform_image::image_preview_decoder() {
+  if (!priv->m_on_ready_call) {
+    qWarning() << Q_FUNC_INFO << "Error : NO callback";
+    priv->m_async_notification_promis.set_value(nullptr);
+    release();
+    return nullptr;
+  }
+
+  bool load_from_cache = priv->check_in_cache(priv->m_file_url);
+
+  io_surface *ck_surface = nullptr;
+  QImage ck_qt_image;
+
+  if (!load_from_cache) {
+    ck_qt_image = QImage(priv->m_file_url.c_str())
+                      .scaled(QSize(128, 128), Qt::KeepAspectRatioByExpanding,
+                              Qt::SmoothTransformation);
+  } else {
+    ck_qt_image.load(priv->cache_file_name(priv->m_file_url));
+  }
+
+  if (!load_from_cache) {
+    QString cache_path =
+        QDir::toNativeSeparators(QDir::homePath() + "/.cherry_io/");
+
+    QString thumbnail_path =
+        QDir::toNativeSeparators(QDir::homePath() + "/.cherry_io/cache/");
+
+    QDir cache_dir(cache_path);
+    QDir thumbnail_dir(thumbnail_path);
+
+    if (!cache_dir.exists()) {
+      if (!QDir::home().mkdir(cache_path)) {
+        priv->m_async_notification_promis.set_value(nullptr);
+        release();
+        return nullptr;
+      }
+    }
+
+    if (!thumbnail_dir.exists()) {
+      if (!QDir::home().mkdir(thumbnail_path)) {
+        qDebug() << Q_FUNC_INFO << "Failed to Create :" << thumbnail_path;
+        priv->m_async_notification_promis.set_value(nullptr);
+        release();
+        return nullptr;
+      }
+    }
+
+    QString cache_file_name = QCryptographicHash::hash(
+        priv->m_file_url.c_str(), QCryptographicHash::Md5).toHex();
+    if (!ck_qt_image.save(
+             QDir::toNativeSeparators(thumbnail_path + "/" + cache_file_name) +
+             ".png")) {
+      qWarning() << Q_FUNC_INFO << "Failed to Create :" << cache_file_name;
+      priv->m_async_notification_promis.set_value(nullptr);
+      release();
+      return nullptr;
+    }
+  }
+
+  if (ck_qt_image.isNull()) {
+    priv->m_async_notification_promis.set_value(nullptr);
+    release();
+    return nullptr;
+  }
+
+  ck_surface = new io_surface();
+  ck_surface->width = ck_qt_image.width();
+  ck_surface->height = ck_qt_image.height();
+
+  ck_surface->buffer =
+      (unsigned char *)malloc(4 * ck_qt_image.height() * ck_qt_image.width());
+
+  for (int x = 0; x < ck_qt_image.height(); ++x) {
+    QRgb *row_data = (QRgb *)ck_qt_image.scanLine(x);
+    for (int y = 0; y < ck_qt_image.width(); ++y) {
+      QRgb pixel_data = row_data[y];
+
+      int red = qRed(pixel_data);
+      int blue = qBlue(pixel_data);
+      int green = qGreen(pixel_data);
+      int alpha = qAlpha(pixel_data);
+
+      int width = ck_surface->width;
+      ck_surface->buffer[((x * width + y) * 4)] = blue;
+      ck_surface->buffer[((x * width + y) * 4) + 1] = green;
+      ck_surface->buffer[((x * width + y) * 4) + 2] = red;
+      ck_surface->buffer[((x * width + y) * 4) + 3] = alpha;
+    }
+  }
+
+  priv->m_async_notification_promis.set_value(ck_surface);
+  release();
+  return ck_surface;
+}
+
+void image_io::platform_image::emit_complete() {
+  io_surface *result = priv->m_async_notification_promis.get_future().get();
+  image_io::buffer_load_status_t status = image_io::kSuccess;
+
+  if (!result)
+    status = image_io::kDataError;
+
+  priv->m_on_ready_call(result, status);
+}
+
+void image_io::platform_image::wait_for_signal(image_io::platform_image *instance) {
+  std::unique_lock<std::mutex> notify_lock(priv->m_notify_lock_mutex);
+  priv->m_notify_condition_variable.wait(
+      notify_lock, [this] { return priv->m_shared_lock_value == 1; });
+  emit_complete();
 }
 
 bool image_io::platform_image::private_platform_image::check_in_cache(
--- extensions/activity/desktop_settings_dialog/desktop_settings_dialog.cpp
+++ extensions/activity/desktop_settings_dialog/desktop_settings_dialog.cpp
@@ -39,51 +39,53 @@
 #include <ck_dial_view.h>
 #include <ck_item_view.h>
 #include <ck_image_io.h>
+
+// stdlib
+#include <condition_variable>
 #include <mutex>
 
 class desktop_settings_dialog::PrivatePhotoSearch {
 public:
-  PrivatePhotoSearch() : m_task_count(0) {}
+  PrivatePhotoSearch() {}
   ~PrivatePhotoSearch() {}
 
-  std::vector<std::string> build_image_list();
+  std::vector<std::string> locate_system_images();
 
   cherry_kit::window *m_window;
   cherry_kit::progress_bar *m_progress_bar_widget;
   cherry_kit::item_view *m_image_view;
   cherry_kit::fixed_layout *m_ck_layout;
 
-  std::vector<QPixmap> m_pixmap_list;
   std::vector<QImage> m_io_surface_list;
   std::vector<cherry_kit::image_io *> m_service_list;
 
-  QRectF mGeometry;
-  QVariantMap mResult;
+  QRectF m_geometry;
+  QVariantMap m_activity_result;
 
-  int m_task_count;
   std::mutex m_task_lock;
 };
 
 desktop_settings_dialog::desktop_settings_dialog(QGraphicsObject *object)
-    : cherry_kit::desktop_dialog(object), d(new PrivatePhotoSearch) {}
+    : cherry_kit::desktop_dialog(object), priv(new PrivatePhotoSearch) {}
 
 desktop_settings_dialog::~desktop_settings_dialog() {
   qDebug() << Q_FUNC_INFO;
-  delete d;
+  delete priv;
 }
 
 void desktop_settings_dialog::create_window(const QRectF &a_window_geometry,
                                             const QString &a_window_title,
                                             const QPointF &window_pos) {
-  d->mGeometry = a_window_geometry;
+  priv->m_geometry = a_window_geometry;
 
-  d->m_window = new cherry_kit::window();
-  d->m_window->setGeometry(a_window_geometry);
-  d->m_window->set_window_title(a_window_title);
+  priv->m_window = new cherry_kit::window();
+  priv->m_window->setGeometry(a_window_geometry);
+  priv->m_window->set_window_title(a_window_title);
 
-  cherry_kit::fixed_layout *ck_ui = new cherry_kit::fixed_layout(d->m_window);
+  cherry_kit::fixed_layout *ck_ui =
+      new cherry_kit::fixed_layout(priv->m_window);
 
-  d->m_ck_layout = ck_ui;
+  priv->m_ck_layout = ck_ui;
 
   ck_ui->set_content_margin(10, 10, 10, 10);
   ck_ui->set_geometry(0, 0, a_window_geometry.width(),
@@ -99,13 +101,13 @@ void desktop_settings_dialog::create_window(const QRectF &a_window_geometry,
   cherry_kit::widget_properties_t ui_data;
 
   cherry_kit::widget *ck_icon_gird = ck_ui->add_widget(0, 0, "widget", ui_data);
-  d->m_image_view = new cherry_kit::item_view(
+  priv->m_image_view = new cherry_kit::item_view(
       ck_icon_gird, cherry_kit::item_view::kGridModel);
   // d->m_image_view->set_content_margin(0, 0, 0, 0);
-  d->m_image_view->set_content_spacing(0);
+  priv->m_image_view->set_content_spacing(0);
 
   ck_icon_gird->on_geometry_changed([&](const QRectF &a_rect) {
-    d->m_image_view->setGeometry(a_rect);
+    priv->m_image_view->setGeometry(a_rect);
   });
 
   ui_data["label"] = "Apply";
@@ -115,59 +117,63 @@ void desktop_settings_dialog::create_window(const QRectF &a_window_geometry,
   ui_data["label"] = "Previous";
   ck_ui->add_widget(1, 2, "button", ui_data);
 
-  d->m_window->set_window_content(ck_ui->viewport());
+  priv->m_window->set_window_content(ck_ui->viewport());
 
-  d->m_image_view->set_view_geometry(ck_icon_gird->geometry());
-  d->m_image_view->setPos(QPointF());
+  priv->m_image_view->set_view_geometry(ck_icon_gird->geometry());
+  priv->m_image_view->setPos(QPointF());
 
-  QTimer::singleShot(500, this, SLOT(load_from_system_path()));
+  QTimer::singleShot(500, this, SLOT(invoke_image_loader()));
   // load_from_system_path();
   exec(window_pos);
 }
 
-QRectF desktop_settings_dialog::geometry() const { return d->mGeometry; }
+QRectF desktop_settings_dialog::geometry() const { return priv->m_geometry; }
 
-QVariantMap desktop_settings_dialog::result() const { return d->mResult; }
+QVariantMap desktop_settings_dialog::result() const { return priv->m_activity_result; }
 
-window *desktop_settings_dialog::dialog_window() const { return d->m_window; }
+window *desktop_settings_dialog::dialog_window() const {
+  return priv->m_window;
+}
 
 void desktop_settings_dialog::cleanup() {
-  if (d->m_image_view)
-    d->m_image_view->clear();
+  if (priv->m_image_view)
+    priv->m_image_view->clear();
 
-  if (d->m_ck_layout)
-    delete d->m_ck_layout;
+  if (priv->m_ck_layout)
+    delete priv->m_ck_layout;
 
-  if (d->m_window) {
-    delete d->m_window;
+  if (priv->m_window) {
+    delete priv->m_window;
   }
 
-  d->m_io_surface_list.erase(std::end(d->m_io_surface_list));
-  d->m_io_surface_list.clear();
+  if (priv->m_io_surface_list.size() > 0) {
+    priv->m_io_surface_list.erase(std::end(priv->m_io_surface_list));
+    priv->m_io_surface_list.clear();
+  }
 
-  std::for_each(std::begin(d->m_service_list), std::end(d->m_service_list),
+  std::for_each(std::begin(priv->m_service_list),
+                std::end(priv->m_service_list),
                 [this](cherry_kit::image_io *a_service) {
-    qDebug() << Q_FUNC_INFO << "clear service";
     if (a_service) {
       delete a_service;
     }
   });
 
-  d->m_service_list.clear();
+  priv->m_service_list.clear();
 
-  d->m_window = 0;
+  priv->m_window = 0;
 }
 
 void desktop_settings_dialog::insert_image_to_grid(
     const QImage &ck_preview_pixmap) const {
 
   if (ck_preview_pixmap.isNull()) {
-    qDebug() << Q_FUNC_INFO << "Null image in list";
+    qWarning() << Q_FUNC_INFO << "Null image in list";
     return;
   }
 
   cherry_kit::image_view *ck_image_preview =
-      new cherry_kit::image_view(d->m_image_view);
+      new cherry_kit::image_view(priv->m_image_view);
 
   ck_image_preview->set_image(ck_preview_pixmap);
 
@@ -192,53 +198,34 @@ void desktop_settings_dialog::insert_image_to_grid(
 
   ck_preview_item->set_view(ck_image_preview);
 
-  d->m_image_view->insert(ck_preview_item);
+  priv->m_image_view->insert(ck_preview_item);
 }
 
 void desktop_settings_dialog::load_images() const {
   std::for_each(
-      std::begin(d->m_io_surface_list), std::end(d->m_io_surface_list),
+      std::begin(priv->m_io_surface_list), std::end(priv->m_io_surface_list),
       [this](const QImage &a_image) { insert_image_to_grid(a_image); });
 }
 
-void desktop_settings_dialog::load_from_system_path() const {
-  std::vector<std::string> current_file_list = d->build_image_list();
-
-  int task_count = current_file_list.size();
-
+void desktop_settings_dialog::invoke_image_loader() const {
+  std::vector<std::string> current_file_list = priv->locate_system_images();
   std::for_each(std::begin(current_file_list), std::end(current_file_list),
                 [=](const std::string &a_file) {
-
     cherry_kit::image_io *ck_image_service = new cherry_kit::image_io(0, 0);
-    d->m_service_list.push_back(ck_image_service);
+    priv->m_service_list.push_back(ck_image_service);
 
     ck_image_service->on_ready([=](
         cherry_kit::image_io::buffer_load_status_t a_status,
         cherry_kit::image_io *a_image_io) {
 
       if (a_status != cherry_kit::image_io::kSuccess) {
-        d->m_task_lock.lock();
-        d->m_task_count++;
-        d->m_task_lock.unlock();
         return;
       }
 
       cherry_kit::io_surface *ck_surface_ref = a_image_io->surface();
-
-      QImage ck_image(ck_surface_ref->buffer, ck_surface_ref->width,
-                      ck_surface_ref->height, QImage::Format_ARGB32);
-
-      // QPixmap pixmap = QPixmap::fromImage(ck_image);
-      d->m_task_lock.lock();
-      // d->m_pixmap_list.push_back(pixmap);
-      d->m_io_surface_list.push_back(ck_image);
-
-      qDebug() << Q_FUNC_INFO << d->m_task_count << "/" << task_count;
-      d->m_task_count++;
-      if (d->m_task_count == task_count) {
-        this->load_images();
-      }
-      d->m_task_lock.unlock();
+      QImage image_buffer(ck_surface_ref->buffer, ck_surface_ref->width,
+                          ck_surface_ref->height, QImage::Format_ARGB32);
+      insert_image_to_grid(image_buffer);
     });
 
     ck_image_service->preview_image(a_file);
@@ -248,7 +235,7 @@ void desktop_settings_dialog::load_from_system_path() const {
 }
 
 std::vector<std::string>
-desktop_settings_dialog::PrivatePhotoSearch::build_image_list() {
+desktop_settings_dialog::PrivatePhotoSearch::locate_system_images() {
   std::vector<std::string> image_path_list;
   std::vector<std::string> rv_list;
 
@@ -272,20 +259,20 @@ desktop_settings_dialog::PrivatePhotoSearch::build_image_list() {
 
     if (path_meta_data.isDir()) {
       QStringList allowed_mime_types;
-      QDir qt_path(path_meta_data.filePath());
+      QDir image_dir(path_meta_data.filePath());
 
       allowed_mime_types << "*.png"
                          << "*.jpg"
                          << "*.jpeg"
                          << "*.tiff"
                          << "*.svg";
-      qt_path.setNameFilters(allowed_mime_types);
+      image_dir.setNameFilters(allowed_mime_types);
 
-      QStringList filtered_local_files = qt_path.entryList();
+      QStringList filtered_local_files = image_dir.entryList();
 
       Q_FOREACH(const QString & image_file_name, filtered_local_files) {
         QString image_full_path = QDir::toNativeSeparators(
-            qt_path.absolutePath() + "/" + image_file_name);
+            image_dir.absolutePath() + "/" + image_file_name);
         rv_list.push_back(image_full_path.toStdString());
       }
     }
