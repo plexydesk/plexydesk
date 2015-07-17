a75c1a2e8c709680ee0102f8b626f58035f67d94
Siraj Razick <siraj@plexydesk.org>
2015-07-17 05:40:19 +0530

    [ui] Clean up the progress widget
    Removes bounding rect and replaces it with content_geometry function
    and uses pain_view instead of directly using pain() from Qt.

--- base/uikit/widgets/ck_progress_bar.cpp
+++ base/uikit/widgets/ck_progress_bar.cpp
@@ -16,167 +16,96 @@ public:
   PrivateProgressBar() : m_width(0), m_height(0) {}
   ~PrivateProgressBar() {}
 
-  QSizeF pixelSizeOfText(const QString &txt);
-  QString mString;
-  float mMaxValue;
-  float mMinValue;
-  float mValue;
-
-  // progress animation
-  QVariantAnimation *mProgressAnimation;
+  float m_maximum_value;
+  float m_minimum_value;
+  float m_current_value;
 
   int m_width;
   int m_height;
 };
 
 progress_bar::progress_bar(widget *parent)
-    : widget(parent), o_progress_bar(new PrivateProgressBar) {
-  o_progress_bar->mMaxValue = 100.0;
-  o_progress_bar->mMinValue = 1.0;
-  o_progress_bar->mValue = (float)0.01;
-  o_progress_bar->m_width = 320;
-  o_progress_bar->m_height = 240;
-
-  o_progress_bar->mProgressAnimation = new QVariantAnimation(this);
-  o_progress_bar->mProgressAnimation->setDuration(100);
-  o_progress_bar->mProgressAnimation->setStartValue(o_progress_bar->mMinValue);
-  o_progress_bar->mProgressAnimation->setEndValue(o_progress_bar->mMaxValue);
-
-  /*
-  set_size(QSize(
-      resource_manager::style()->attribute("widget", "line_edit_width").toInt(),
-      resource_manager::style()
-          ->attribute("widget", "line_edit_height")
-          .toInt()));
-
-  */
-  connect(o_progress_bar->mProgressAnimation, SIGNAL(valueChanged(QVariant)),
-          this, SLOT(valueChanged(QVariant)));
-
-  setFlag(QGraphicsItem::ItemIsMovable, false);
+    : widget(parent), priv(new PrivateProgressBar) {
+  priv->m_maximum_value = 100.0;
+  priv->m_minimum_value = 1.0;
+  priv->m_current_value = (float)0.01;
+  priv->m_width = 320;
+  priv->m_height = 240;
 }
 
 progress_bar::~progress_bar() {
   qDebug() << Q_FUNC_INFO;
-  delete o_progress_bar;
-}
-
-void progress_bar::set_label(const QString &a_txt) {
-  o_progress_bar->mString = a_txt;
-  Q_EMIT contentBoundingRectChaned();
-}
-
-/*
-     this is a comment
-     */
-
-QString progress_bar::text() const { return o_progress_bar->mString; }
-
-QRectF progress_bar::boundingRect() const {
-  return QRectF(0, 0, o_progress_bar->m_width, o_progress_bar->m_height);
+  delete priv;
 }
 
 void progress_bar::set_size(const QSizeF &size) {
-  o_progress_bar->m_width = size.width();
-  o_progress_bar->m_height = size.height();
+  priv->m_width = size.width();
+  priv->m_height = size.height();
   setGeometry(QRectF(0, 0, size.width(), size.height()));
   update();
 }
 
 QSizeF progress_bar::sizeHint(Qt::SizeHint which,
                               const QSizeF &a_constraint) const {
-  return QSizeF(o_progress_bar->m_width, o_progress_bar->m_height);
+  return QSizeF(priv->m_width, priv->m_height);
 }
 
-int progress_bar::max_range() { return o_progress_bar->mMaxValue; }
+QRectF progress_bar::contents_geometry() const {
+  return QRectF(0, 0, priv->m_width, priv->m_height);
+}
 
-int progress_bar::min_range() { return o_progress_bar->mMinValue; }
+int progress_bar::max_range() { return priv->m_maximum_value; }
+
+int progress_bar::min_range() { return priv->m_minimum_value; }
 
 void progress_bar::set_range(int a_min, int a_max) {
-  o_progress_bar->mMinValue = a_min;
-  o_progress_bar->mMaxValue = a_max;
-
-  o_progress_bar->mProgressAnimation->setStartValue(o_progress_bar->mMinValue);
-  o_progress_bar->mProgressAnimation->setEndValue(o_progress_bar->mMaxValue);
-
+  priv->m_minimum_value = a_min;
+  priv->m_maximum_value = a_max;
   update();
 }
 
 void progress_bar::set_value(int a_value) {
-  if (a_value > o_progress_bar->mMaxValue) {
-    o_progress_bar->mValue = o_progress_bar->mMaxValue;
-    o_progress_bar->mProgressAnimation->setEndValue(o_progress_bar->mValue);
-    o_progress_bar->mProgressAnimation->start();
-    update();
-    return;
-  }
-
-  if (a_value < o_progress_bar->mMinValue) {
-    o_progress_bar->mValue = o_progress_bar->mMinValue;
-    o_progress_bar->mProgressAnimation->setEndValue(o_progress_bar->mValue);
-    o_progress_bar->mProgressAnimation->start();
-    update();
-    return;
-  }
-
-  o_progress_bar->mValue = a_value;
-  o_progress_bar->mProgressAnimation->setEndValue(o_progress_bar->mValue);
-  o_progress_bar->mProgressAnimation->start();
+  if (a_value > priv->m_maximum_value) {
+    priv->m_current_value = priv->m_maximum_value;
+  } else if (a_value < priv->m_minimum_value) {
+    priv->m_current_value = priv->m_minimum_value;
+  } else
+    priv->m_current_value = a_value;
   update();
 }
 
 int progress_bar::value() const {
-  float percentage = (o_progress_bar->mValue / o_progress_bar->mMaxValue) * 100;
+  float percentage = (priv->m_current_value / priv->m_maximum_value) * 100;
 
   return percentage;
 }
 
 void progress_bar::on_value_changed(const QVariant &a_value) {
-  o_progress_bar->mValue = a_value.toFloat();
+  priv->m_current_value = a_value.toFloat();
   update();
 }
 
-void progress_bar::mouseReleaseEvent(QGraphicsSceneMouseEvent *a_event_ptr) {
-  widget::mouseReleaseEvent(a_event_ptr);
-}
-
-void progress_bar::paint(QPainter *a_painter_ptr,
-                         const QStyleOptionGraphicsItem *a_option_ptr,
-                         QWidget *a_widget_ptr) {
-  qDebug() << Q_FUNC_INFO << a_option_ptr->exposedRect;
-  a_painter_ptr->save();
+void progress_bar::paint_view(QPainter *a_painter_ptr, const QRectF &a_rect) {
   style_data features;
   features.render_state = style_data::kRenderBackground;
-  features.geometry = a_option_ptr->exposedRect.adjusted(10, 0, -24, 0);
+  features.geometry = a_rect.adjusted(10, 0, -24, 0);
 
   if (cherry_kit::resource_manager::style()) {
     cherry_kit::resource_manager::style()->draw("linear_progress_bar", features,
                                                 a_painter_ptr);
   }
 
-  float percentage = (o_progress_bar->mValue / o_progress_bar->mMaxValue) * 100;
-  float progressLevel =
-      ((a_option_ptr->exposedRect.width() - 24) / 100) * percentage;
+  float percentage_completed =
+      (priv->m_current_value / priv->m_maximum_value) * 100;
+  float current_progress = ((a_rect.width() - 24) / 100) * percentage_completed;
 
-  style_data progressFeatures;
-  progressFeatures.geometry =
-      QRectF(10.0, 0.0, progressLevel, a_option_ptr->exposedRect.height());
+  features.geometry = QRectF(10.0, 0.0, current_progress, a_rect.height());
 
-  progressFeatures.render_state = style_data::kRenderForground;
+  features.render_state = style_data::kRenderForground;
 
   if (cherry_kit::resource_manager::style()) {
-    cherry_kit::resource_manager::style()->draw(
-        "linear_progress_bar", progressFeatures, a_painter_ptr);
+    cherry_kit::resource_manager::style()->draw("linear_progress_bar", features,
+                                                a_painter_ptr);
   }
-
-  a_painter_ptr->restore();
-}
-
-QSizeF progress_bar::PrivateProgressBar::pixelSizeOfText(const QString &txt) {
-  QFont font("Times", 20, QFont::Bold);
-  QFontMetrics metic(font);
-  QRectF rect = metic.boundingRect(txt);
-
-  return rect.size();
 }
 }
--- base/uikit/widgets/ck_progress_bar.h
+++ base/uikit/widgets/ck_progress_bar.h
@@ -31,12 +31,10 @@ public:
   explicit progress_bar(widget *a_parent_ptr = 0);
   virtual ~progress_bar();
 
-  virtual void set_label(const QString &a_txt);
-  virtual QString text() const;
-
   virtual void set_size(const QSizeF &a_size);
+
   virtual QSizeF sizeHint(Qt::SizeHint which, const QSizeF &a_constraint) const;
-  virtual QRectF boundingRect() const;
+  virtual QRectF contents_geometry() const;
 
   virtual int max_range();
   virtual int min_range();
@@ -48,18 +46,12 @@ public:
 
   void on_value_changed(const QVariant &a_value);
 
-Q_SIGNALS:
-  void contentBoundingRectChaned();
-
 protected:
-  virtual void mouseReleaseEvent(QGraphicsSceneMouseEvent *a_event_ptr);
-  virtual void paint(QPainter *a_painter_ptr,
-                     const QStyleOptionGraphicsItem *a_option_ptr,
-                     QWidget *a_widget_ptr = 0);
+  virtual void paint_view(QPainter *a_painter_ptr, const QRectF &a_rect);
 
 private:
   class PrivateProgressBar;
-  PrivateProgressBar *const o_progress_bar;
+  PrivateProgressBar *const priv;
 };
 }
 
