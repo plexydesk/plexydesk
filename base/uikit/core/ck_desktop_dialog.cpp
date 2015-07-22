f23d63ec2382a0d9759b28898090aa48f2b0bf7e
Siraj Razick <siraj@plexydesk.org>
2015-07-23 00:56:19 +0530

    [ui] adds support for sending messages from dialogs.
    Adds a new callback to send messages to the dialog host.

--- base/uikit/core/ck_desktop_dialog.cpp
+++ base/uikit/core/ck_desktop_dialog.cpp
@@ -31,31 +31,30 @@ public:
   m_action_completed_list;
   QList<std::function<void(const desktop_dialog *)> > m_discard_handler_list;
   std::future<void> m_async_notification_result;
+
+  std::vector<dialog_message_t> m_notify_chain;
 };
 
 desktop_dialog::desktop_dialog(QGraphicsObject *parent)
-    : QObject(parent), o_desktop_dialog(new PrivateDesktopActivity) {
-  o_desktop_dialog->m_current_viewport = 0;
+    : QObject(parent), priv(new PrivateDesktopActivity) {
+  priv->m_current_viewport = 0;
 }
 
-desktop_dialog::~desktop_dialog() { delete o_desktop_dialog; }
+desktop_dialog::~desktop_dialog() { delete priv; }
 
 void desktop_dialog::set_activity_attribute(const QString &a_name,
                                             const QVariant &a_data) {
-  o_desktop_dialog->m_arguments[a_name] = a_data;
+  priv->m_arguments[a_name] = a_data;
 }
 
-QVariantMap desktop_dialog::attributes() const {
-  return o_desktop_dialog->m_arguments;
-}
+QVariantMap desktop_dialog::attributes() const { return priv->m_arguments; }
 
 void desktop_dialog::update_attribute(const QString &a_name,
                                       const QVariant &a_data) {
   set_activity_attribute(a_name, a_data);
   Q_EMIT attribute_changed();
 
-  foreach(std::function<void()> l_handler,
-          o_desktop_dialog->m_arg_handler_list) {
+  foreach(std::function<void()> l_handler, priv->m_arg_handler_list) {
     if (l_handler) {
       l_handler();
     }
@@ -65,17 +64,17 @@ void desktop_dialog::update_attribute(const QString &a_name,
 QString desktop_dialog::error_message() const { return QString(); }
 
 void desktop_dialog::set_geometry(const QRectF &a_geometry) {
-  o_desktop_dialog->m_geometry = a_geometry;
+  priv->m_geometry = a_geometry;
 
   if (dialog_window()) {
     dialog_window()->setGeometry(a_geometry);
   }
 }
 
-QRectF desktop_dialog::geometry() const { return o_desktop_dialog->m_geometry; }
+QRectF desktop_dialog::geometry() const { return priv->m_geometry; }
 
 bool desktop_dialog::has_attribute(const QString &a_arg) {
-  return o_desktop_dialog->m_arguments.keys().contains(a_arg);
+  return priv->m_arguments.keys().contains(a_arg);
 }
 
 void desktop_dialog::exec(const QPointF &a_pos) {
@@ -92,7 +91,7 @@ void desktop_dialog::show_activity() {
 
 void desktop_dialog::discard_activity() {
   foreach(std::function<void(const desktop_dialog *)> func,
-          o_desktop_dialog->m_discard_handler_list) {
+          priv->m_discard_handler_list) {
     if (func) {
       func(this);
     }
@@ -113,47 +112,49 @@ void desktop_dialog::hide() {
 
 void
 desktop_dialog::set_controller(const desktop_controller_ref &a_controller) {
-  o_desktop_dialog->m_controller_ptr = a_controller;
+  priv->m_controller_ptr = a_controller;
 }
 
 desktop_controller_ref desktop_dialog::controller() const {
-  return o_desktop_dialog->m_controller_ptr;
+  return priv->m_controller_ptr;
 }
 
 void desktop_dialog::set_viewport(space *a_viewport_ptr) {
-  if (o_desktop_dialog->m_current_viewport) {
+  if (priv->m_current_viewport) {
     return;
   }
 
-  o_desktop_dialog->m_current_viewport = a_viewport_ptr;
+  priv->m_current_viewport = a_viewport_ptr;
 }
 
-space *desktop_dialog::viewport() const {
-  return o_desktop_dialog->m_current_viewport;
-}
+space *desktop_dialog::viewport() const { return priv->m_current_viewport; }
 
 void desktop_dialog::on_arguments_updated(std::function<void()> a_handler) {
-  o_desktop_dialog->m_arg_handler_list.append(a_handler);
+  priv->m_arg_handler_list.append(a_handler);
 }
 
 void desktop_dialog::on_action_completed(
     std::function<void(const QVariantMap &)> a_handler) {
-  o_desktop_dialog->m_action_completed_list.append(a_handler);
+  priv->m_action_completed_list.append(a_handler);
 }
 
 void desktop_dialog::on_discarded(
     std::function<void(const desktop_dialog *)> a_handler) {
-  o_desktop_dialog->m_discard_handler_list.append(a_handler);
+  priv->m_discard_handler_list.append(a_handler);
+}
+
+void desktop_dialog::on_notify(dialog_message_t callback) {
+  priv->m_notify_chain.push_back(callback);
 }
 
 void desktop_dialog::update_action() {
-  if (!o_desktop_dialog->m_controller_ptr.data()) {
+  if (!priv->m_controller_ptr.data()) {
     qWarning() << Q_FUNC_INFO << "Error: Controller Not Set";
     return;
   }
 
-  o_desktop_dialog->m_controller_ptr->request_action(
-      result()["action"].toString(), result());
+  priv->m_controller_ptr->request_action(result()["action"].toString(),
+                                         result());
 }
 
 // todo: remove this
@@ -167,16 +168,25 @@ void desktop_dialog::update_content_geometry(widget *a_widget_ptr) {
 
 void desktop_dialog::notify_done() {
   foreach(std::function<void(const QVariantMap & l_data)> l_func,
-          o_desktop_dialog->m_action_completed_list) {
+          priv->m_action_completed_list) {
     if (l_func) {
       l_func(result());
     }
   }
 
-  o_desktop_dialog->m_async_notification_result =
-      std::async(std::launch::async, [this]() {
-        if (dialog_window())
-          dialog_window()->close();
-      });
+  priv->m_async_notification_result = std::async(std::launch::async, [this]() {
+    if (dialog_window())
+      dialog_window()->close();
+  });
+}
+
+void desktop_dialog::notify_message(const std::string &a_key,
+                                    const std::string &a_value) {
+  std::for_each(std::begin(priv->m_notify_chain),
+                std::end(priv->m_notify_chain), [&](dialog_message_t a_func) {
+
+    if (a_func)
+      a_func(a_key, a_value);
+  });
 }
 }
