efee77577784d0d61ca9b96f3903cefa14fe208e
Siraj Razick <siraj@plexydesk.org>
2015-07-28 19:41:56 +0530

    [test] Adds a test runtime to detect problems.
    New test runtime mock_desktop to detect and fix problems.

---
+++ extensions/mock_desk/extrunner.h
@@ -0,0 +1,27 @@
+#ifndef EXT_LOADERTESTRUNNER_H
+#define EXT_LOADERTESTRUNNER_H
+
+#include <QObject>
+#include <QVariantMap>
+
+class ExtLaoderTestRunner : public QObject {
+  Q_OBJECT
+public:
+  explicit ExtLaoderTestRunner(QObject *a_parent_ptr = 0);
+
+  virtual ~ExtLaoderTestRunner();
+
+  bool connectToDataSource(const QString &source);
+
+  void loadtest(const QString &source);
+
+public
+slots:
+  void onSourceUpdated(const QVariantMap &data);
+
+private:
+  class PrivateExtLaoderTestRunner;
+  PrivateExtLaoderTestRunner *const d;
+};
+
+#endif // EXT_LOADERTESTRUNNER_H
