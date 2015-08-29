5ace4938c41f552a518cd1623756eabab55a48c6
Siraj Razick <siraj@plexydesk.org>
2015-08-29 12:47:58 +0530

    [ui] Functinal Timer class.
    Adds the missing impl to the timer class, currently it's just a wrapper
    around QTimer. But we should write a better one when we have our own
    event loop.

--- base/uikit/core/ck_timer.h
+++ base/uikit/core/ck_timer.h
@@ -1,11 +1,26 @@
 #ifndef CK_TIMER_H
 #define CK_TIMER_H
 
+#include <functional>
+#include <chrono>
+
 namespace cherry_kit {
 class timer {
 public:
   timer(int a_duration);
   virtual ~timer();
+
+  virtual void start() const;
+  virtual void stop();
+
+  int elapsed_seconds();
+  int elapsed_minutes();
+
+  virtual bool is_active() const;
+
+  static void start_once(int duration, std::function<void ()> a_callback);
+  virtual void on_timeout(std::function<void()> a_callback);
+
 private:
   class private_timer;
   private_timer * const priv;
