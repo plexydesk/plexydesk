9a5ada14ee3c1d82d72b136a55026d0dd0814c3d
Siraj Razick <siraj@kde.org>
2015-10-18 05:48:48 +0530

    [plugin][date] Add Start time and end time instead of duration.
    Adds the option of setting the start time and the duration in the
    appoinment dialog.

--- extensions/widgets/date/date.cpp
+++ extensions/widgets/date/date.cpp
@@ -261,6 +261,7 @@ date_controller::create_ui_calendar_ui(cherry_kit::session_sync *a_session) {
   ui->add_segments(0, 1);
   ui->add_segments(1, 1);
   ui->add_segments(2, 1);
+
   ui->set_row_height(0, "50%");
   ui->set_row_height(1, "45%");
   ui->set_row_height(2, "5%");
@@ -480,30 +481,47 @@ cherry_kit::window *time_segment::create_new() {
   cherry_kit::window *ck_window = new cherry_kit::window();
   cherry_kit::fixed_layout *ck_layout = new cherry_kit::fixed_layout(ck_window);
   cherry_kit::line_edit *ck_duration_editor = 0;
+  cherry_kit::line_edit *ck_end_time_editor = 0;
   cherry_kit::text_editor *ck_note_editor = 0;
+
+  ck_window->set_window_title("Add Appointment");
 
   ck_layout->set_verticle_spacing(5);
   ck_layout->set_content_margin(5, 5, 5, 5);
-  ck_layout->set_geometry(0, 0, 240, 148);
-  ck_layout->add_rows(3);
-  ck_layout->add_segments(0, 4);
-  ck_layout->add_segments(1, 1);
-  ck_layout->add_segments(2, 3);
+  ck_layout->set_geometry(0, 0, 320, 240);
 
-  ck_layout->set_row_height(0, "20%");
-  ck_layout->set_row_height(1, "60%");
-  ck_layout->set_row_height(2, "20%");
+  ck_layout->add_rows(4);
+
+  ck_layout->add_segments(0, 4);
+  ck_layout->add_segments(1, 4);
+  ck_layout->add_segments(2, 1);
+  ck_layout->add_segments(3, 3);
+
+  ck_layout->set_row_height(0, "10%");
+  ck_layout->set_row_height(1, "10%");
+  ck_layout->set_row_height(2, "70%");
+  ck_layout->set_row_height(3, "10%");
 
   ck_layout->set_segment_width(0, 0, "25%");
   ck_layout->set_segment_width(0, 1, "55%");
   ck_layout->set_segment_width(0, 2, "10%");
   ck_layout->set_segment_width(0, 3, "10%");
 
+  ck_layout->set_segment_width(1, 0, "25%");
+  ck_layout->set_segment_width(1, 1, "55%");
+  ck_layout->set_segment_width(1, 2, "10%");
+  ck_layout->set_segment_width(1, 3, "10%");
+
   time_event *event = new time_event();
 
   cherry_kit::widget_properties_t ui_data;
 
-  ui_data["label"] = "Duration";
+  ui_data["text"] = "";
+  ck_note_editor = dynamic_cast<cherry_kit::text_editor *>(
+      ck_layout->add_widget(2, 0, "text_edit", ui_data));
+
+  /* start adding first row of the layout */
+  ui_data["label"] = "Start";
   ck_layout->add_widget(0, 0, "label", ui_data);
 
   ui_data["text"] = std::to_string(m_time_value) + ":00";
@@ -511,10 +529,6 @@ cherry_kit::window *time_segment::create_new() {
   ck_duration_editor = dynamic_cast<cherry_kit::line_edit *>(
       ck_layout->add_widget(0, 1, "line_edit", ui_data));
   ck_duration_editor->set_readonly(true);
-
-  ui_data["text"] = "";
-  ck_note_editor = dynamic_cast<cherry_kit::text_editor *>(
-      ck_layout->add_widget(1, 0, "text_edit", ui_data));
 
   ui_data["label"] = "+";
   ck_layout->add_widget(0, 2, "button", ui_data, [=]() {
@@ -550,9 +564,54 @@ cherry_kit::window *time_segment::create_new() {
         QString("%1:%2").arg(m_time_value).arg(time_value));
   });
 
+  /* end adding the first row */
+  ui_data["label"] = "End";
+  ck_layout->add_widget(1, 0, "label", ui_data);
+
+  ui_data["text"] = std::to_string(m_time_value) + ":00";
+
+  ck_end_time_editor = dynamic_cast<cherry_kit::line_edit *>(
+      ck_layout->add_widget(1, 1, "line_edit", ui_data));
+  ck_end_time_editor->set_readonly(true);
+
+  ui_data["label"] = "+";
+  ck_layout->add_widget(1, 2, "button", ui_data, [=]() {
+    m_duration++;
+
+    if (m_duration >= 60)
+      m_duration = 60;
+
+    event->set_duration(m_duration);
+    QString time_value = QString("%1").arg(m_duration);
+    if (m_duration <= 9) {
+      time_value = QString("0%1").arg(m_duration);
+    }
+
+    ck_end_time_editor->set_text(
+        QString("%1:%2").arg(m_time_value).arg(time_value));
+  });
+
+  ui_data["label"] = "-";
+  ck_layout->add_widget(1, 3, "button", ui_data, [=]() {
+    m_duration--;
+    QString time_value = QString("%1").arg(m_duration);
+
+    if (m_duration <= 0)
+      m_duration = 0;
+
+    event->set_duration(m_duration);
+    if (m_duration <= 9) {
+      time_value = QString("0%1").arg(m_duration);
+    }
+
+    ck_end_time_editor->set_text(
+        QString("%1:%2").arg(m_time_value).arg(time_value));
+  });
+
+  //////
   ui_data["label"] = "Add";
   cherry_kit::button *ck_add_btn = dynamic_cast<cherry_kit::button *>(
-      ck_layout->add_widget(2, 1, "button", ui_data));
+      ck_layout->add_widget(3, 1, "button", ui_data));
 
   ck_add_btn->on_click([=]() {
     event->set_note(ck_note_editor->text().toStdString());
