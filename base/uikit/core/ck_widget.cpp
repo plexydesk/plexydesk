ea00108bcc511eaf1ce88d383a96e9175bb1ae63
Siraj Razick <siraj@plexydesk.org>
2015-11-01 02:05:25 +0530

    [ui] Fix rendering on Non Apple computers.
    Tweaks rendering on none apple computers.

--- base/uikit/core/ck_widget.cpp
+++ base/uikit/core/ck_widget.cpp
@@ -63,7 +63,7 @@ widget::widget(widget *parent)
   priv->m_name = QLatin1String("Widget");
   priv->m_current_layer_type = kRenderAtForgroundLevel;
 
-  setCacheMode(ItemCoordinateCache);
+  setCacheMode(DeviceCoordinateCache);
   setAcceptedMouseButtons(Qt::LeftButton | Qt::RightButton);
 
   setFlag(QGraphicsItem::ItemIsMovable, false);
