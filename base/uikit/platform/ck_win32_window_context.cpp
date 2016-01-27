570b4c3ef3963a17a97357bb871fdc7db2974dc8
Siraj Razick <siraj@plexydesk.com>
2016-01-27 12:23:28 +0530

    [windows] Adds a stub windows window manager code.
    Stub code for windows window management interface.

--- base/uikit/platform/ck_win32_window_context.cpp
+++ base/uikit/platform/ck_win32_window_context.cpp
@@ -1,7 +1,17 @@
 #include "ck_win32_window_context.h"
 
+namespace cherry_kit {
 win32_window_context::win32_window_context()
 {
-
 }
 
+win32_window_context::~win32_window_context()
+{
+}
+
+device_window *win32_window_context::find_window(const std::string &a_title) {
+  device_window *rv = 0;
+  return rv;
+}
+
+}
