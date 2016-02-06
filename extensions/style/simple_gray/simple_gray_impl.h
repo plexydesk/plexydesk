ba9d60e0609c192369f630b410a25d92caefb629
Siraj Razick <siraj@kde.org>
2016-02-07 02:04:22 +0530

    [plugin] New PlexyDesk Style
    New Stub Style for PlexyDesk

--- extensions/style/simple_gray/simple_gray_impl.h
+++ extensions/style/simple_gray/simple_gray_impl.h
@@ -16,23 +16,29 @@
 *  You should have received a copy of the GNU General Public License
 *  along with PlexyDesk. If not, see <http://www.gnu.org/licenses/lgpl.html>
 *******************************************************************************/
-#ifndef UBUNTU_DATA_I
-#define UBUNTU_DATA_I
+#ifndef COCOA_DATA_I
+#define COCOA_DATA_I
 
 #include <QtCore>
 
 #include <ck_style_interface.h>
 
-class ubuntuInterface : public QObject, public PlexyDesk::WidgetStyleInterface {
+class SimpleGrayInterface : public QObject, public cherry_kit::style_interface {
   Q_OBJECT
-  Q_INTERFACES(PlexyDesk::WidgetStyleInterface)
-  Q_PLUGIN_METADATA(IID "org.qt-project.ubuntu")
+  Q_INTERFACES(cherry_kit::style_interface)
+  Q_PLUGIN_METADATA(IID "org.qt-project.simplegray")
 
 public:
-  virtual ~ubuntuInterface() {}
+  virtual ~SimpleGrayInterface() {
+        qDebug() << Q_FUNC_INFO;
+        m_instence_ref.clear();
+    }
 
   /* this will return a valid data plugin pointer*/
-  QSharedPointer<PlexyDesk::Style> style();
+  QSharedPointer<cherry_kit::style> style();
+
+private:
+  QSharedPointer<cherry_kit::style> m_instence_ref;
 };
 
 #endif
