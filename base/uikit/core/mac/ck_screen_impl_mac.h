5e98d89ea701bf9bab6abd8943b993b5c287e935
Siraj Razick <siraj@plexydesk.com>
2016-03-18 14:30:25 +0530

    [mac] Move desktop size calculation to ck_screen.
    Move screen size calculation from workspace to screen class.

--- base/uikit/core/mac/ck_screen_impl_mac.h
+++ base/uikit/core/mac/ck_screen_impl_mac.h
@@ -6,18 +6,18 @@
 namespace cherry_kit {
 class screen::platform_screen {
 public:
-    platform_screen();
-    virtual ~platform_screen();
+  platform_screen();
+  virtual ~platform_screen();
 
-    int screen_count() const;
+  int screen_count() const;
 
-    float scale_factor(int a_display_id) const;
+  float scale_factor(int a_display_id) const;
 
-    float desktop_width() const;
-    float desktop_height() const;
+  float desktop_width(int a_display_id) const;
+  float desktop_height(int a_display_id) const;
 
-    float display_width(int a_display_id) const;
-    float display_height(int a_display_id) const;
+  float display_width(int a_display_id) const;
+  float display_height(int a_display_id) const;
 };
 }
 
