9ef49a2c12855015d8bed9c98df4d769fc025fe6
Siraj Razick <siraj@Sirajs-iMac.local>
2016-03-20 11:01:43 +0530

    [plugin][dock] New Dock
    New Vertical dock class to handle Dock functions better.

---
+++ extensions/widgets/dock/vertical_dock.h
@@ -0,0 +1,24 @@
+#ifndef VERTICAL_DOCK_H
+#define VERTICAL_DOCK_H
+
+#include <ck_fixed_layout.h>
+#include <ck_ui_action.h>
+#include <ck_widget.h>
+#include <ck_window.h>
+
+class vertical_dock {
+public:
+  vertical_dock();
+  virtual ~vertical_dock();
+
+  cherry_kit::window *window();
+
+  void create_dock_action(int row, int column, const std::string &icon,
+                          std::function<void()> a_button_action_func);
+
+private:
+  cherry_kit::window *m_window;
+  cherry_kit::fixed_layout *m_fixed_layout;
+};
+
+#endif // VERTICAL_DOCK_H
