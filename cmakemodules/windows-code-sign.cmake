8535439fec22ffe464dd54f822ed3a8f4446a95a
Siraj Razick <siraj@plexydesk.com>
2016-05-20 10:41:25 -0700

    [build] Disable Code signing in Developer build.
    Disable Code signing in windows developer build.

--- cmakemodules/windows-code-sign.cmake
+++ cmakemodules/windows-code-sign.cmake
@@ -1,4 +1,5 @@
 macro(windows_code_sign_target arg ext)
+if (NOT ENABLE_DEVELOPER_BUILD)
 add_custom_command(TARGET ${arg} POST_BUILD
                     COMMAND ${SIGNTOOL} sign /f
                     ${CODE_SIGN_CERT}
@@ -7,14 +8,17 @@ add_custom_command(TARGET ${arg} POST_BUILD
                     "/d" "PlexyDesk - Desktop NG Desktop Widgets"
                     ${arg}.${ext}
                   )
+endif()
 endmacro()
 
 
 macro(windows_code_sign_deps arg)
+if (NOT ENABLE_DEVELOPER_BUILD)
   include(${CMAKE_BINARY_DIR}/CPackConfig.cmake)
   set(BUILD_ROOT "${CMAKE_BINARY_DIR}/_CPack_Packages/${CPACK_SYSTEM_NAME}/WIX/${CPACK_PACKAGE_FILE_NAME}/${arg}")
   file(TO_CMAKE_PATH ${CODE_SIGN_CERT} OUT_VALUE)
   file (TO_NATIVE_PATH ${BUILD_ROOT} TARGET_SIGN_FILE)
   STRING(REGEX REPLACE "\\\\" "\\\\\\\\" GOOD_PATH ${TARGET_SIGN_FILE})
   install(CODE "EXECUTE_PROCESS(COMMAND \"${SIGNTOOL}\" sign /f ${OUT_VALUE} /p ${CODE_SIGN_CERT_AUTH} /t \"http://timestamp.comodoca.com/authenticode\" ${GOOD_PATH})")
+endif()
 endmacro()
