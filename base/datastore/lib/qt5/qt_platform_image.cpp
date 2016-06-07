dfa46ede39e90b96abbda3c096d9ab42465dcb6b
Siraj Razick <siraj@plexydesk.com>
2016-06-07 21:13:02 +0530

    [data] Support saving image to disk.
    Adds support for saving image to disk, this allows us to save
    the wallpapers we download from pixabay.

--- base/datastore/lib/qt5/qt_platform_image.cpp
+++ base/datastore/lib/qt5/qt_platform_image.cpp
@@ -1,32 +1,37 @@
 #include "qt_platform_image.h"
-#include <QDebug>
-#include <QImage>
-#include <QDir>
-#include <QCryptographicHash>
 #include <QCoreApplication>
+#include <QCryptographicHash>
+#include <QDebug>
+#include <QDir>
+#include <QImage>
 
-#include <thread>
 #include <future>
+#include <thread>
 
 namespace cherry_kit {
 
 class image_io::platform_image::private_platform_image {
 public:
-  private_platform_image() {}
+  private_platform_image() : m_current_surface(0){}
   ~private_platform_image() {}
 
   bool check_in_cache(const std::string &a_file_name);
   QString cache_file_name(const std::string &a_file_name);
 
   std::function<void(io_surface *, image_io::buffer_load_status_t)>
-  m_on_ready_call;
+      m_on_ready_call;
   std::string m_file_url;
   std::thread m_io_task;
 
+	io_surface *m_current_surface;
   std::future<io_surface *> m_async_task_result;
+  std::future<std::string> m_async_data_image_url;
 
   std::condition_variable m_notify_condition_variable;
   std::mutex m_notify_lock_mutex;
+
+  std::vector<on_save_callback_t> m_notify_save_list;
+
   int m_shared_lock_value;
 };
 
@@ -42,6 +47,8 @@ void image_io::platform_image::load_from_file(const std::string &a_file_name) {
   QCoreApplication::processEvents();
 
   io_surface *result = priv->m_async_task_result.get();
+	priv->m_current_surface = result;
+
   image_io::buffer_load_status_t status = image_io::kSuccess;
 
   if (!result)
@@ -50,8 +57,8 @@ void image_io::platform_image::load_from_file(const std::string &a_file_name) {
   priv->m_on_ready_call(result, status);
 }
 
-void
-image_io::platform_image::load_image_preview(const std::string &a_file_name) {
+void image_io::platform_image::load_image_preview(
+    const std::string &a_file_name) {
   priv->m_file_url = a_file_name;
   priv->m_async_task_result =
       std::async(std::launch::async,
@@ -92,13 +99,78 @@ void image_io::platform_image::load_from_buffer(const unsigned char *a_buffer,
   result->width = _qimage.width();
 
   status = image_io::kSuccess;
-
   priv->m_on_ready_call(result, status);
 }
 
 void image_io::platform_image::on_surface_ready(
     std::function<void(io_surface *, buffer_load_status_t)> a_callback) {
   priv->m_on_ready_call = a_callback;
+}
+
+void image_io::platform_image::on_save_ready(on_save_callback_t a_callback) {
+	priv->m_notify_save_list.push_back(a_callback);
+}
+
+void image_io::platform_image::save_completed(const std::string &a_file_name) {
+	std::for_each(priv->m_notify_save_list.begin(),
+			priv->m_notify_save_list.end(),[&] (on_save_callback_t a_func) {
+			 if (a_func)
+			   a_func(a_file_name);
+			});
+}
+
+void image_io::platform_image::save_image(const io_surface *a_surface,
+		const std::string &a_prefix) {
+	std::string rv;
+
+	if (!a_surface || a_surface->buffer == 0) {
+		std::cout << "Null Image Data -> " <<  __FUNCTION__ << std::endl;
+	  save_completed(rv);
+		return;
+	}
+
+	std::string target_file;
+
+	QDir file_system;
+	QFile target_device;
+	QString target_dir;
+	QCryptographicHash crypto(QCryptographicHash::Md5);
+	QImage qimage = QImage(a_surface->buffer,
+			a_surface->width, a_surface->height, QImage::Format_ARGB32); 
+
+	crypto.addData((const char *)a_surface->buffer,
+			(a_surface->width * a_surface->height * 4)); 
+	
+	std::string data_hash(crypto.result().toHex());
+
+	target_file = QDir::homePath().toStdString() + "/.socialkit/cache/" 
+		+ "/" + a_prefix + "/";
+	target_dir = QString::fromStdString(target_file); 
+
+  file_system = QDir(target_dir);
+
+	if (!file_system.exists()) {
+		file_system.mkpath(target_dir);
+	}
+	
+	target_file += data_hash + ".png";
+
+	std::cout << __FUNCTION__ << "target : " << target_file << std::endl;
+
+	target_device.setFileName(QDir::toNativeSeparators(
+			QString::fromStdString(target_file)));
+
+	if (target_device.open(QIODevice::WriteOnly)) {
+	  qimage.save(&target_device, "PNG", 100);
+	}
+
+	save_completed(target_file);
+}
+
+void image_io::platform_image::save(const io_surface *a_surface,
+		const std::string &a_prefix) {
+	std::cout << "Request -> " <<  __FUNCTION__ << std::endl;
+	save_image(a_surface, a_prefix);
 }
 
 void image_io::platform_image::release() {
@@ -193,11 +265,12 @@ io_surface *image_io::platform_image::image_preview_decoder() {
       }
     }
 
-    QString cache_file_name = QCryptographicHash::hash(
-        priv->m_file_url.c_str(), QCryptographicHash::Md5).toHex();
+    QString cache_file_name = QCryptographicHash::hash(priv->m_file_url.c_str(),
+                                                       QCryptographicHash::Md5)
+                                  .toHex();
     if (!ck_qt_image.save(
-             QDir::toNativeSeparators(thumbnail_path + "/" + cache_file_name) +
-             ".png")) {
+            QDir::toNativeSeparators(thumbnail_path + "/" + cache_file_name) +
+            ".png")) {
       qWarning() << Q_FUNC_INFO << "Failed to Create :" << cache_file_name;
       return nullptr;
     }
@@ -251,8 +324,8 @@ void image_io::platform_image::emit_complete() {
   priv->m_on_ready_call(result, status);
 }
 
-void
-image_io::platform_image::wait_for_signal(image_io::platform_image *instance) {
+void image_io::platform_image::wait_for_signal(
+    image_io::platform_image *instance) {
   std::unique_lock<std::mutex> notify_lock(priv->m_notify_lock_mutex);
   priv->m_notify_condition_variable.wait(
       notify_lock, [this] { return priv->m_shared_lock_value == 1; });
@@ -261,8 +334,9 @@ image_io::platform_image::wait_for_signal(image_io::platform_image *instance) {
 
 bool image_io::platform_image::private_platform_image::check_in_cache(
     const std::string &a_file_name) {
-  QString hashed_name = QCryptographicHash::hash(
-      a_file_name.c_str(), QCryptographicHash::Md5).toHex();
+  QString hashed_name =
+      QCryptographicHash::hash(a_file_name.c_str(), QCryptographicHash::Md5)
+          .toHex();
   QString thumbnail_path =
       QDir::toNativeSeparators(QDir::homePath() + "/.cherry_io/cache/");
 
@@ -275,8 +349,9 @@ bool image_io::platform_image::private_platform_image::check_in_cache(
 
 QString image_io::platform_image::private_platform_image::cache_file_name(
     const std::string &a_file_name) {
-  QString hashed_name = QCryptographicHash::hash(
-      a_file_name.c_str(), QCryptographicHash::Md5).toHex();
+  QString hashed_name =
+      QCryptographicHash::hash(a_file_name.c_str(), QCryptographicHash::Md5)
+          .toHex();
   QString thumbnail_path =
       QDir::toNativeSeparators(QDir::homePath() + "/.cherry_io/cache/");
 
