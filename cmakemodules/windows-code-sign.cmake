9d2cebb07ad4aa124d2586ee35f10c36128bc007
Siraj Razick <siraj@plexydesk.com>
2016-12-07 12:06:34 -0800

    [windows ] Update certifiacte signing code for windows.
    Remove Sha1 signing and replace it with sha256 when code signing
    the windows installer.

--- cmakemodules/windows-code-sign.cmake
+++ cmakemodules/windows-code-sign.cmake
@@ -1,12 +1,8 @@
 macro(windows_code_sign_target arg ext)
 if (NOT ENABLE_DEVELOPER_BUILD)
-add_custom_command(TARGET ${arg} POST_BUILD
-                    COMMAND ${SIGNTOOL} sign /f
-                    ${CODE_SIGN_CERT}
-                    "/p" ${CODE_SIGN_CERT_AUTH}
-                    "/t" "http://timestamp.comodoca.com/authenticode"
-                    "/d" "PlexyDesk - Desktop NG Desktop Widgets"
-                    ${arg}.${ext}
+	file(TO_CMAKE_PATH ${CODE_SIGN_CERT} OUT_VALUE)
+	add_custom_command(TARGET ${arg} POST_BUILD
+		COMMAND ${SIGNTOOL} sign /p ${CODE_SIGN_CERT_AUTH} /f "${OUT_VALUE}" /td sha256 -fd sha256 /tr "http://timestamp.comodoca.com/authenticode" /d "PlexyDesk - Desktop NG Desktop Widgets" ${arg}.${ext}
                   )
 endif()
 endmacro()
@@ -19,6 +15,6 @@ if (NOT ENABLE_DEVELOPER_BUILD)
   file(TO_CMAKE_PATH ${CODE_SIGN_CERT} OUT_VALUE)
   file (TO_NATIVE_PATH ${BUILD_ROOT} TARGET_SIGN_FILE)
   STRING(REGEX REPLACE "\\\\" "\\\\\\\\" GOOD_PATH ${TARGET_SIGN_FILE})
-  install(CODE "EXECUTE_PROCESS(COMMAND \"${SIGNTOOL}\" sign /f ${OUT_VALUE} /p ${CODE_SIGN_CERT_AUTH} /t \"http://timestamp.comodoca.com/authenticode\" ${GOOD_PATH})")
+  install(CODE "EXECUTE_PROCESS(COMMAND \"${SIGNTOOL}\" sign /f ${OUT_VALUE} /p ${CODE_SIGN_CERT_AUTH} /td sha256 /fd sha256 /tr \"http://timestamp.comodoca.com/authenticode\" ${GOOD_PATH})")
 endif()
 endmacro()
