32d823aa5549244f2bc83f7fa321ac2b11bbd356
Siraj Razick <siraj@storekit.org>
2017-02-23 21:54:16 -0500

    [social] New Test Case Local services.
    New Test Case for Local Services of PlexyDesk

---
+++ base/social/testrunner/test_url_post_request.cpp
@@ -0,0 +1,5 @@
+#include "test_url_post_request.h"
+
+test_url_post_request::test_url_post_request()
+{
+}
---
+++ base/social/testrunner/test_url_post_request.h
@@ -0,0 +1,10 @@
+#ifndef TEST_URL_POST_REQUEST_H
+#define TEST_URL_POST_REQUEST_H
+
+class test_url_post_request
+{
+public:
+  test_url_post_request();
+};
+
+#endif // TEST_URL_POST_REQUEST_H
