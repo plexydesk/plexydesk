1aaacd0bafb6fd8e5e7a6d8560b4e7e8718678a0
siraj Razick <siraj@plexydesk.com>
2017-03-30 14:41:55 -0400

    [ui] Use setGeometry instead of move for Native Editor
    When using the Qt editor switch to setGeometry instead of moving it.
    carbon has a problem.

--- base/uikit/widgets/ck_text_editor.cpp
+++ base/uikit/widgets/ck_text_editor.cpp
@@ -149,8 +149,9 @@ void text_editor::set_geometry(const QRectF &a_rect) {
   priv->m_proxy_widget->setMaximumSize(a_rect.size());
   priv->m_proxy_widget->resize(a_rect.size());
   priv->m_text_editor->resize(a_rect.width(), a_rect.height());
-  priv->m_text_editor->move(0.0, 0.0);
-
+  priv->m_text_editor->setGeometry(QRect(0, 0, a_rect.width(), a_rect.height()));
+  //priv->m_text_editor->setPos(0.0, 0.0);
+ 
   widget::set_geometry(a_rect);
   update_scrollbars();
 }
